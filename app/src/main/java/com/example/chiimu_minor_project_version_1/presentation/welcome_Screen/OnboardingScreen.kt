ec99a416b05c572fe554991fcf68f7f3eb6326b2
Harsh Rastogi <harshrastogi013@gmail.com>
2024-04-10 18:22:00 +0530

    .

--- app/src/main/java/com/example/chiimu_minor_project_version_1/presentation/welcome_Screen/OnboardingScreen.kt
+++ app/src/main/java/com/example/chiimu_minor_project_version_1/presentation/welcome_Screen/OnboardingScreen.kt
@@ -29,7 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
