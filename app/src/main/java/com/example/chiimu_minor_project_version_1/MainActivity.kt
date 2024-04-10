0e6e14b6dc186dbc936a474282fb128d8dd2c9fd
Harsh Rastogi <harshrastogi013@gmail.com>
2024-04-10 22:38:23 +0530

    errors were solved.

--- app/src/main/java/com/example/chiimu_minor_project_version_1/MainActivity.kt
+++ app/src/main/java/com/example/chiimu_minor_project_version_1/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.chiimu_minor_project_version_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.chiimu_minor_project_version_1.navigation.NavigationGraph
 import com.example.chiimu_minor_project_version_1.ui.theme.Chiimu_minor_project_version_1Theme
 import dagger.hilt.android.AndroidEntryPoint
